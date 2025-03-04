691cf64a31ff6fc98a15c91c70770a5483fdff28
ZaidKH987 <zaid.khateb@gmail.com>
2025-03-04 09:06:40 +0300

    mario applying

--- Classes/JS/mario.js
+++ Classes/JS/mario.js
@@ -1,19 +1,58 @@
 function move(direction)
 {
-    var id=document.getElementById('icon')
-    if(direction=='left'){
-        console.log(direction)  
+var id=document.getElementById('icon');
+var ho=id.style.left;
+var ver=id.style.bottom;
+
+
+
+
+
+
+
+    if(ho=='')
+        {
+            ho=0;
+           
+        }
+
         
+
+if(ver=='')
+    {
+        ver=0;
     }
-      
-        if(direction=='right'){
-            console.log()}
 
-            if(direction=='up'){
-                console.log()}
 
-                if(direction=='down'){
-                    console.log()}
+if(direction=='left')
+{
+ho=parseInt(ho) - 10;
+id.style.left=ho+ 'px';
+console.log(ho);
+}
 
-     
+
+if(direction=='right')
+{
+    ho=parseInt(ho)+ 10;
+    id.style.left=ho+'px';
+    console.log(ho);
+}
+
+
+
+if(direction=='up')
+    {
+        ver=parseInt(ver)+ 10;
+        id.style.bottom=ver+'px';
+        console.log(ver);
+    }
+
+    if(direction=='down')
+        {
+            ver=parseInt(ver)- 10;
+            id.style.bottom=ver+'px';
+            console.log(ver);
+        }
+
 }
